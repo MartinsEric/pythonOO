1cd70cb054fbba24ab7991942b536911c2caa6ac
Eric Martins <ericmartins.martins@gmail.com>
2018-10-05 20:07:06 -0300

    tornando atributo privado e adicionando likes

--- modelo.py
+++ modelo.py
@@ -1,13 +1,15 @@
 class Filme:
 
     def __init__(self, nome, ano, duracao):
-        self.nome = nome
+        self.__nome = nome
         self.ano = ano
         self.duracao = duracao
+        self.__likes = 0
 
 class Serie:
 
     def __init__(self, nome, ano, temporadas):
-        self.nome = nome
+        self.__nome = nome
         self.ano = ano
         self.temporadas = temporadas
+        self.__likes = 0
